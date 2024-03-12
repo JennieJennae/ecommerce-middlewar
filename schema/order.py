952a23564502961d03bb66c151debee835a173d6
Jennifer <chinonyejennie@gmail.com>
2024-03-12 08:59:11 +0100

    Updated files

--- schema/order.py
+++ schema/order.py
@@ -1,18 +1,29 @@
 from typing import Union
 from pydantic import BaseModel
 from schema.customer import Customer
+from enum import Enum
 
 from schema.product import Product
 
+class Status(str, Enum):
+    pending = 'Pending'
+    completed = 'Completed'
+      
 class Order(BaseModel):
     id: int
     customer_id: Union[int, Customer]
     items: list[int]
+    status: Status = Status.pending
 
 class OrderCreate(BaseModel):
     customer_id: int
-    items: list[int | Product]
+    items: list[int]
+    status: Status = Status.pending
 
-orders: list[Order] = [
-    Order(id=1, customer_id=1, items=[1, 2])
+class CheckoutOrder(BaseModel):
+    status: Status 
+
+
+orders = [
+    Order(id=1, customer_id=1, items=[1, 2, 3], status = Status.pending)
 ]
