e2721bc2515c2f8cf820df672d42b1088da7d453
Jennifer <chinonyejennie@gmail.com>
2024-03-12 09:03:02 +0100

    Updates

--- routers/order.py
+++ routers/order.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,status, HTTPException
 
-from schema.order import Order, OrderCreate, orders
+from schema.order import Order, OrderCreate, orders, CheckoutOrder, Status
 from services.order import order_service
 
 order_router = APIRouter()
@@ -10,6 +10,7 @@ order_router = APIRouter()
 
 @order_router.get('/', status_code=200)
 def list_orders():
+    response = []
     response = order_service.order_parser(orders)
     return {'message': 'success', 'data': response}
 
@@ -23,7 +24,21 @@ def create_order(payload: OrderCreate = Depends(order_service.check_availability
         id=order_id,
         customer_id=customer_id,
         items=product_ids,
+        status = Status.pending
     )
     orders.append(new_order)
     return {'message': 'Order created successfully', 'data': new_order}
+
+@order_router.put('/{order_id}', status_code=status.HTTP_202_ACCEPTED)
+def checkout_order(order_id: int, payload: Status):
+    check = None
+    for order in orders:
+        if order.id == order_id:
+            check = order
+            break
+    if not check:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="The order id does not exist")
+    check.status = payload  
+    return {"message": "The order has been completed", "data": {"order_id": check.id, "status": check.status}}
+
 
